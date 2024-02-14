a9494c75471c25fb6b316ddc49db325e883fcb90
takeli99 <sergiotakeli@gmail.com>
2024-02-14 18:33:48 +0100

    comento el codigo

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
-const TelegramBot = require('node-telegram-bot-api');
-const fetch = require('node-fetch');
+const TelegramBot = require('node-telegram-bot-api'); // Llamo al bot
+const fetch = require('node-fetch'); // Para subir la API
 
-const token = '6685306159:AAFAEZc7RrG3BI7EN-MjNJPN1pJg5i1_MCU';
+const token = '6685306159:AAFAEZc7RrG3BI7EN-MjNJPN1pJg5i1_MCU'; // Token de mi bot
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {polling: true}); // Creo el bot
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Bienvenido al test de la casa de Harry Potter, ¿estás listo para comenzar?');
-});
+}); // Mensaje de bienvenida
 
 
 
