475b937706767e26e573e3498d5a5f023d26dedd
takeli99 <sergiotakeli@gmail.com>
2024-02-19 11:44:54 +0100

    Finalizo el bot, pongo el token en otro sitio (más seguridad), usuario elige las preguntas

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 let fetch;
+const config = require( './config.json' );
+const token = config.token;
 
 try {
     fetch = require('node-fetch');
@@ -7,117 +9,234 @@ try {
     console.error('Error al importar node-fetch:', err);
     return;
 }
+const bot = new TelegramBot(token, {polling: true}); // Creo el bot
 
 function iniciarBot() {
-    const token = '6685306159:AAFAEZc7RrG3BI7EN-MjNJPN1pJg5i1_MCU'; // Token de mi bot
-    const bot = new TelegramBot(token, {polling: true}); // Creo el bot
-
     const respuestasUsuario = {}; // Creo un objeto para guardar las respuestas
 
-    bot.onText(/\/start/, (msg) => {
-        const chatId = msg.chat.id;
+    bot.onText(/\/start/, (message) => {
+        const chatId = message.chat.id;
         bot.sendMessage(chatId, 'Bienvenido al test de la casa de Harry Potter, ¿estás listo para comenzar?');
-        preguntarCualidad(msg);
     }); // Mensaje de bienvenida
 
+    bot.onText(/si/i, (message) => { // El bot empieza con el usuario pone si
+        preguntarCualidad(message);
+    });
+
     // Funciones para preguntar cualidad, mascota, color, asignatura, lugar, hechizo, objeto mágico y mascota mágica
-    function preguntarCualidad(msg) {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '¿Cuál de estas cualidades consideras más importante?\n1) Valentía\n2) Lealtad\n3) Sabiduría\n4) Ambición');
+    function preguntarCualidad(message) {
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, '¿Cuál de estas cualidades consideras más importante?', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: '1) Valentía', callback_data: '1' }],
+                    [{ text: '2) Lealtad', callback_data: '2' }],
+                    [{ text: '3) Sabiduría', callback_data: '3' }],
+                    [{ text: '4) Ambición', callback_data: '4' }]
+                ]
+            }
+        });
     }
 
-    function preguntarMascota(msg) {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '¿Qué mascota te gustaría tener?\n1) Lechuza\n2) Gato\n3) Rata\n4) Sapo');
+    function preguntarMascota(message) {
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, '¿Qué mascota te gustaría tener?',  {
+            reply_markup: { 
+                inline_keyboard: [
+                    [{ text: '1) Lechuza', callback_data: '1' }],
+                    [{ text: '2) Gato', callback_data: '2' }],
+                    [{ text: '3) Rata', callback_data: '3' }],
+                    [{ text: '4) Sapo', callback_data: '4' }]
+                ]
+            }
+        });
     }
 
-    function preguntarColor(msg) {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '¿Cuál de estos colores te gusta más?\n1) Rojo\n2) Amarillo\n3) Azul\n4) Verde');
+    function preguntarColor(message) {
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, '¿Cuál de estos colores te gusta más?', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: '1) Rojo', callback_data: '1' }],
+                    [{ text: '2) Amarillo', callback_data: '2' }],
+                    [{ text: '3) Azul', callback_data: '3' }],
+                    [{ text: '4) Verde', callback_data: '4' }],
+                ]
+            }
+        });
     }
 
-    function preguntarAsignatura(msg){
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '¿Cuál de estas asignaturas te gustaría estudiar?\n1) Pociones\n2) Transformaciones\n3) Encantamientos\n4) Defensa contra las artes oscuras');
+    function preguntarAsignatura(message){
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, '¿Cuál de estas asignaturas te gustaría estudiar?', {
+            reply_markup:{
+                inline_keyboard: [
+                    [{ text: '1) Pociones', callback_data: '1' }],
+                    [{ text: '2) Transformaciones', callback_data: '2' }],
+                    [{ text: '3) Ecantamientos', callback_data: '3' }],
+                    [{ text: '4) Defensa contra las artes oscuras', callback_data: '4' }],
+                ]
+            }
+        });
     }
 
-    function preguntarLugar(msg) {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '¿Qué lugar mágico te gustaría visitar?\n1) Hogsmeade\n2) El Callejón Diagon\n3) La Sala de los Menesteres\n4) La Madriguera');
+    function preguntarLugar(message) {
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, '¿Qué lugar mágico te gustaría visitar?', {
+            reply_markup : {
+                inline_keyboard: [
+                    [{ text: '1) Hogsmeade', callback_data: '1' }],
+                    [{ text: '2) El callejon Diagon', callback_data: '2' }],
+                    [{ text: '3) La sala de los Menesteres', callback_data: '3' }],
+                    [{ text: '4) La Madriguera', callback_data: '4' }],
+                ]
+            }
+        });
     }
 
-    function preguntarHechizo(msg) {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '¿Cuál de estos hechizos te gustaría aprender?\n1) Expecto Patronum\n2) Wingardium Leviosa\n3) Lumos\n4) Expelliarmus');
+    function preguntarHechizo(message) {
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, '¿Cuál de estos hechizos te gustaría aprender?', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: '1) Expecto Patronum', callback_data: '1' }],
+                    [{ text: '2) Wingardium Leviosa', callback_data: '2' }],
+                    [{ text: '3) Lumos', callback_data: '3' }],
+                    [{ text: '4) Expelliarmus', callback_data: '4' }],
+                ]
+            }
+        });
     }
 
-    function preguntarObjetoMagico(msg) {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '¿Cuál de estos objetos mágicos te gustaría tener?\n1) La capa de invisibilidad\n2) La varita de saúco\n3) La piedra de la resurrección\n4) El giratiempo');
+    function preguntarObjetoMagico(message) {
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, '¿Cuál de estos objetos mágicos te gustaría tener?', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: '1) La capa de invisibilidad', callback_data: '1' }],
+                    [{ text: '2) La varita de saúco', callback_data: '2' }],
+                    [{ text: '3) La piedradela resurrección', callback_data: '3' }],
+                    [{ text: '4) El giratiempo', callback_data: '4' }],
+                ]
+            }
+        });
     }
 
-    function preguntarMascotaMagica(msg) {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '¿Cuál sería tu mascota mágica ideal?\n1) Dragón\n2) Hipogrifo\n3) Fenix\n4) Elfo doméstico');
+    function preguntarMascotaMagica(message) {
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, '¿Cuál sería tu mascota mágica ideal?', {
+            reply_markup: { 
+                inline_keyboard: [
+                    [{ text: '1) Dragón', callback_data: '1' }],
+                    [{ text: '2) Hipogrifo', callback_data: '2'}],
+                    [{ text: '3) Fenix', callback_data: '3' }],
+                    [{ text: '4) Elfo doméstico', callback_data: '4' }],
+                ] 
+            },
+        });
     }
 
-    bot.on('message', (msg) => {
-        const chatId = msg.chat.id;
-        const messageText = msg.text.toLowerCase();
-        const userId = msg.from.id;
+    bot.on('callback_query', (callbackQuery) => {
+        const message = callbackQuery.message;
+        const userId = message.from.id;
+        const data = callbackQuery.data;
 
         if (!respuestasUsuario[userId]) {
-            // Si no hay respuestas, preguntar por la cualidad
-            preguntarCualidad(msg);
-            respuestasUsuario[userId] = { cualidad: null, mascota: null, color: null, asignatura: null, lugar: null, hechizo: null, objetoMagico: null, mascotaMagica: null };
-        } else if (!respuestasUsuario[userId].cualidad) {
-            // Si aún no ha respondido a la primera pregunta, guardar la respuesta y preguntar por la mascota
-            respuestasUsuario[userId].cualidad = messageText;
-            preguntarMascota(msg);
-        } else if (!respuestasUsuario[userId].mascota) {
-            // Si aún no ha respondido a la segunda pregunta, guardar la respuesta y preguntar por el color
-            respuestasUsuario[userId].mascota = messageText;
-            preguntarColor(msg);
-        } else if (!respuestasUsuario[userId].color) {
-            // Si aún no ha respondido a la tercera pregunta, guardar la respuesta y preguntar por la asignatura favorita
-            respuestasUsuario[userId].color = messageText;
-            preguntarAsignatura(msg);
-        } else if (!respuestasUsuario[userId].asignatura) {
-            // Si aún no ha respondido a la cuarta pregunta, guardar la respuesta y preguntar por el lugar mágico favorito
-            respuestasUsuario[userId].asignatura = messageText;
-            preguntarLugar(msg);
-        } else if (!respuestasUsuario[userId].lugar) {
-            // Si aún no ha respondido a la quinta pregunta, guardar la respuesta y preguntar por el tipo de hechizo favorito
-            respuestasUsuario[userId].lugar = messageText;
-            preguntarHechizo(msg);
-        } else if (!respuestasUsuario[userId].hechizo) {
-            // Si aún no ha respondido a la sexta pregunta, guardar la respuesta y preguntar por el objeto mágico favorito
-            respuestasUsuario[userId].hechizo = messageText;
-            preguntarObjetoMagico(msg);
-        } else if (!respuestasUsuario[userId].objetoMagico) {
-            // Si aún no ha respondido a la séptima pregunta, guardar la respuesta y preguntar por la mascota mágica ideal
-            respuestasUsuario[userId].objetoMagico = messageText;
-            preguntarMascotaMagica(msg);
-        } else if (!respuestasUsuario[userId].mascotaMagica) {
-            // Si aún no ha respondido a la octava pregunta, guardar la respuesta y determinar la casa
-            respuestasUsuario[userId].mascotaMagica = messageText;
-            
-            // Llamar a la API Sorting Hat con las respuestas del usuario
-            determinarCasa(chatId, respuestasUsuario[userId]);
+            preguntarCualidad(message);
+            respuestasUsuario[userId] = [];
+        } else if (respuestasUsuario[userId].length < 8) {
+            respuestasUsuario[userId].push(data);
+            switch (respuestasUsuario[userId].length) {
+                case 1:
+                    preguntarMascota(message);
+                    break;
+                case 2:
+                    preguntarColor(message);
+                    break;
+                case 3:
+                    preguntarAsignatura(message);
+                    break;
+                case 4:
+                    preguntarLugar(message);
+                    break;
+                case 5:
+                    preguntarHechizo(message);
+                    break;
+                case 6:
+                    preguntarObjetoMagico(message);
+                    break;
+                case 7:
+                    preguntarMascotaMagica(message);
+                    break;
+                case 8:
+                    determinarCasa(message.chat.id, respuestasUsuario[userId]);
+                    break;
+                default:
+                    break;
+            }
         }
     });
 
-    bot.onText(/\/casa/, (msg) => {
-        const chatId = msg.chat.id;
-        fetch('https://www.potterapi.com/v1/sortingHat') // Api del sombrero que te dice tu casa en Hogwarts
-            .then(response => response.json())
-            .then(data => {
-                bot.sendMessage(chatId, `¡Basado en el sombrero seleccionador, tu casa en Hogwarts es: ${data}!`);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                bot.sendMessage(chatId, 'Lo siento, ha ocurrido un error al determinar tu casa. Por favor, inténtalo de nuevo más tarde.');
-            });
+    bot.onText(/\/casa/, (message) => {
+        const chatId = message.chat.id;
+        bot.sendMessage(chatId, 'Primero completa el test para conocer tu casa.');
     });
 }
+
+function determinarCasa(chatId, respuestasUsuario) {
+    let gryffindor = 0;
+    let hufflepuff = 0;
+    let ravenclaw = 0;
+    let slytherin = 0;
+
+    respuestasUsuario.forEach(respuesta => {
+        switch (respuesta) {
+            case '1':
+                gryffindor++;
+                break;
+            case '2':
+                hufflepuff++;
+                break;
+            case '3':
+                ravenclaw++;
+                break;
+            case '4':
+                slytherin++;
+                break;
+            default:
+                break;
+        }
+    });
+
+    let max = Math.max(gryffindor, hufflepuff, ravenclaw, slytherin);
+
+    let casa = '';
+    if (max === gryffindor) {
+        casa = 'Gryffindor';
+    } else if (max === hufflepuff) {
+        casa = 'Hufflepuff';
+    } else if (max === ravenclaw) {
+        casa = 'Ravenclaw';
+    } else {
+        casa = 'Slytherin';
+    }
+
+    bot.sendMessage(chatId, `¡Basado en el sombrero seleccionador, tu casa en Hogwarts es: ${casa}!`);
+}
+
+iniciarBot();
+
+    // bot.onText(/\/casa/, (message) => {
+    //     const chatId = message.chat.id;
+    //     fetch('https://www.potterapi.com/v1/sortingHat') // Api del sombrero que te dice tu casa en Hogwarts
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             bot.sendMessage(chatId, `¡Basado en el sombrero seleccionador, tu casa en Hogwarts es: ${data}!`);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error:', error);
+    //             bot.sendMessage(chatId, 'Lo siento, ha ocurrido un error al determinar tu casa. Por favor, inténtalo de nuevo más tarde.');
+    //         });
+    // }); COMO LA API YA LA HAN QUITADO LO HE HECHO A MANO
+
 
